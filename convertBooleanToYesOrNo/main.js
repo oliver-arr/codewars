744f587021320cd6848033991579b470b8e8e732
oliver-arr <oarroyo.developer@gmail.com>
2019-08-21 20:11:25 +0100

    ternary operator implemented

---
+++ convertBooleanToYesOrNo/main.js
@@ -0,0 +1,14 @@
+/*
+Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+true ==> yes
+false ==> no
+*/
+
+
+function boolToWord( bool ){
+    return bool ? "Yes" : "No";
+  }
+
+  console.log(boolToWord(true));
+  console.log(boolToWord(false));
