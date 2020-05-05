5bc76d07d1e55b7609416c26c2de9cf7d691bec2
oliver-arr <oarroyo.developer@gmail.com>
2020-05-05 21:41:01 +0100

    revert

--- are-they-the-same/index.js
+++ are-they-the-same/index.js
@@ -1,22 +1,19 @@
-function comp(array1, array2) {
-  const array2Copy = [...array2];
-
-  array1.forEach((number) => {
-    let matchedIndex = array2Copy.indexOf(number * number);
-
-    if (matchedIndex > -1) {
-      array2Copy.splice(matchedIndex, 1);
-    }
-  });
-
-  return !array2Copy.length >= 1;
-}
+function comp(array1, array2) {}
 
 const a1 = [121, 144, 19, 161, 19, 144, 19, 11];
-const a2 = [121, 14641, 20736, 361, 25921, 361, 20736, 361];
+const a2 = [
+  11 * 11,
+  121 * 121,
+  144 * 144,
+  19 * 19,
+  161 * 161,
+  19 * 19,
+  144 * 144,
+  19 * 19,
+];
 
 const b1 = [121, 144, 19, 161, 19, 144, 19, 11];
 const b2 = [121, 14641, 20736, 36100, 25921, 361, 20736, 361];
 
 console.log(comp(a1, a2)); // true
-console.log(comp(b1, b2)); // false: 36100 is not the square of any number in b1
+console.log(comp(b1, b2)); // false
