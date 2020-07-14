52d6c9345405ee3d6453dfda40617f6811ca712e
oliver-arr <oarroyo.developer@gmail.com>
2020-07-14 15:13:51 +0100

    refactored

--- count-characters-in-string/index.js
+++ count-characters-in-string/index.js
@@ -2,18 +2,12 @@ const count = (string) => {
   const characterCounter = {};
 
   string.split("").forEach((character) => {
-    if (Object.keys(characterCounter).includes(character)) {
-      characterCounter[character] += 1;
-    } else {
-      characterCounter[character] = 1;
-    }
+    characterCounter[character]
+      ? characterCounter[character]++
+      : (characterCounter[character] = 1);
   });
 
   return characterCounter;
 };
-
-console.log(count("a"));
-console.log(count("aba"));
-console.log(count("hello world"));
 
 module.exports = count;
